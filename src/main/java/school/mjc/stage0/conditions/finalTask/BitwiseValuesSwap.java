4d5cc661f77e3a54c1202cc7b09e27be57166ec8
Sasha-PC\Sasha <aleksandr.v.g.m@gmail.com>
2022-07-29 21:16:14 +0300

    solution Module #4.1. Conditions.Final Task

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        first = first^second;
+        second = first^second;
+        first = first^second;
+        System.out.println(first);
+        System.out.println(second);
     }
+
+    public static void main(String [] avrs){
+        int first = 183;
+        int second = 5;
+
+        System.out.println(first^second^second);
+    }
+
 }
